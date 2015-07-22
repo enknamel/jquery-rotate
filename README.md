446d27f958c5595cce3ece2554368d948dfd0ee8
daniel <enknamel@gmail.com>
2015-07-21 17:36:03 -0700

    making the rotation cumulative

--- README.md
+++ README.md
@@ -13,5 +13,7 @@ bower install jquery-rotate
 
 ```javascript
 $('#element').rotate(90); // rotate element to 90 degrees
+$('#element').rotate(90); // rotate element another 90 degrees
+$('#element').setRotation(90); // set the rotation of the elemnt to 90 degrees
 $('#element').rotate(); // random rotate
 ```
--- jquery.rotate.js
+++ jquery.rotate.js
@@ -1,33 +1,40 @@
 /**
- * jquery-rotate v0.1.0
+ * jquery-rotate v0.0.1
  *  Very lightweight jquery rotate plugin using CSS 3 Transformation
- *  https://github.com/schickling/jquery-rotate
+ *  https://github.com/enknamel/jquery-rotate
  */
 (function ($) {
 
-	$.fn.extend({
-		rotate: function (deg) {
+    $.fn.extend({
+        rotate: function (deg) {
+            //this will literally rotate including any previous rotation from this plugin
+            var $this = $(this);
+            var rotation = (($this.data('jquery-rotation') || 0) + deg) % 360;
+            $this.setRotation(rotation)
+        },
+        setRotation: function (deg) {
+            // cache dom element
+            var $this = $(this);
 
-			// cache dom element
-			var $this = $(this);
+            // make deg random if not set
+            if (deg === null) {
+                deg = Math.floor(Math.random() * 359);
+            }
 
-			// make deg random if not set
-			if (deg === null) {
-				deg = Math.floor(Math.random() * 359);
-			}
+            $this.data('jquery-rotation', deg);
 
-			// rotate dom element
-			$this.css({
-				'-webkit-transform': 'rotate(' + deg + 'deg)',
-				'-moz-transform': 'rotate(' + deg + 'deg)',
-				'-ms-transform': 'rotate(' + deg + 'deg)',
-				'-o-transform': 'rotate(' + deg + 'deg)',
-				'transform': 'rotate(' + deg + 'deg)'
-			});
+            // rotate dom element
+            $this.css({
+                '-webkit-transform': 'rotate(' + deg + 'deg)',
+                '-moz-transform': 'rotate(' + deg + 'deg)',
+                '-ms-transform': 'rotate(' + deg + 'deg)',
+                '-o-transform': 'rotate(' + deg + 'deg)',
+                'transform': 'rotate(' + deg + 'deg)'
+            });
 
-			// make chainable
-			return $this;
-		}
-	});
+            // make chainable
+            return $this;
+        }
+    });
 
 })(jQuery);
